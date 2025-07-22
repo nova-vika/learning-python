9c7c480990d0f3bba9d0c2f054f5062eade3ea25
Vikashini <ivikashinisuresh@gmail.com>
2025-07-22 16:29:19 +0530

    origin

---
+++ 1st day of python + DSA learning.py
@@ -0,0 +1,83 @@
+#variables 
+Name ="vikas"
+print(Name)
+
+#Datatypes
+ #integer
+a=10
+print(type(a))
+
+ #string
+name="rakshana"
+print(type(name))
+
+#float
+height= 157.6
+print(type(height))
+
+#boolean
+is_student=True
+print(type(is_student))
+
+#list
+fruits=["apple","banana","grapes"]
+print(type(fruits))
+
+#tuple
+colors=("pink","blue")
+print(type(colors))
+
+#set
+whole_numbers={0,1,2}
+print(type(whole_numbers))
+
+#dictionary
+student = {"name": "vikas", "age": 18}
+print(type(student))
+
+# DSA concept in list
+# slove problem given by chat gpt
+fav_foods=["biriyani","parota","maagi"]
+print(fav_foods[1])
+#input:
+name=input("enter your name:")
+print("hello",name)
+
+# DSA intermediate level list operation:
+#append - add item at the end
+my_list = ["apple", "dosa","chicken"]
+my_list.append("chapatti")
+print(my_list)
+
+#remove
+my_list = ["apple", "dosa","chicken"]
+my_list.remove("chicken")
+print(my_list)
+
+#insert:
+my_list = ["apple", "dosa","chicken"]
+my_list.insert(1,"curry dosa")
+print(my_list)
+
+#pop:
+my_list = ["apple", "dosa","chicken"]
+my_list.pop(1)
+print(my_list)
+
+#sort
+numbers=[5,4,3,2,1,0]
+numbers.sort()
+print(numbers)
+
+#reverse:
+numbers=[5,4,3,2,1,0]
+numbers.reverse()
+print(numbers)
+
+#index
+colors=["pink","blue","violet"]
+print(colors.index("pink"))
+  
+ #len
+colors=["pink","blue","violet"]
+print(len(colors))
