b4d055d92d24e18fd247ae3cf6be7c65a274ee06
Vikashini <ivikashinisuresh@gmail.com>
2025-07-24 13:50:19 +0530

    Arithmetic Operators in python

---
+++ Arithmetic operators.py
@@ -0,0 +1,20 @@
+# ARITHMETIC OPERATORS:
+#     + , - , * , / , // , % , ** :
+
+a=2
+b=7
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+# if follows BODMAS rule:
+# examples:
+
+print(10-5*3)
+print(20-10/5*2)
+print(2**3**(2%10))
