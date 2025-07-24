3babc740fa2f6f1aa4879c1c07378ffdad2eb486
Vikashini <ivikashinisuresh@gmail.com>
2025-07-24 14:25:51 +0530

    Relational operators

---
+++ Relational Operators.py
@@ -0,0 +1,31 @@
+# RELATIONAL OPERATORS : 
+#      == , != , > , < , >= , <= 
+#      The is is either TRUE or FALSE 
+
+# compare using integer :
+
+a = 20 
+b = 23
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+# compare using string :
+# how means using their ASCII VALUE 
+# it coverts the letters into their ascii value and then compare 
+
+a = "vikas"
+b = "Vikas"        # V = 86
+                   # v = 118
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
