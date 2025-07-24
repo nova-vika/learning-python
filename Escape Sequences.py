ccae22d7fca9c58a2c26c33b81a06ff042665862
Vikashini <ivikashinisuresh@gmail.com>
2025-07-24 12:25:56 +0530

    escape sequences in python

---
+++ Escape Sequences.py
@@ -0,0 +1,22 @@
+# Escape sequences :
+
+#\n :
+print("hello\nworld")
+
+#\t:
+print("hello\tworld")
+
+#\\:
+print("hello\\world")
+
+#\':
+print("It\'s great!")
+
+#\":
+print("he said\"hii\"")
+
+#TRICK:
+# instead of using \' and \" , use single and double quotes vicesly .
+
+print("it's great!")
+print('he said"hiii!')
