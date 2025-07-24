82aabe7ab84245d59f73be840c717c23a3b95f79
Vikashini <ivikashinisuresh@gmail.com>
2025-07-24 15:35:47 +0530

    Argumented Assignment Operator

---
+++ Argumented Assignment Operator.py
@@ -0,0 +1,29 @@
+ # Argumentend assignment operator:
+    
+    # these are the  shortcuts that combaine an operation with assignment .
+    # used for save time and looks cleaner.
+
+a = 10
+                          # its like :
+                               # a = a + 10
+                               # a = 10 + 10 = 20
+a += 10
+print(a)
+
+a -= 10
+print(a)  
+
+a *= 10
+print(a)
+
+a /= 10
+print(a)
+
+a //= 10 
+print(a)
+
+a %= 10 
+print(a)
+
+a **= 10
+print(a)
