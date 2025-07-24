853f2e574ede1bd12ac62cea97da9d4e48ec4fa3
Vikashini <ivikashinisuresh@gmail.com>
2025-07-24 16:14:02 +0530

    Membership Operator

---
+++ Membership Operator.py
@@ -0,0 +1,42 @@
+# Membership operator :
+    #  >> in
+    #  >> not in
+
+#>>>>>>>>>>>>> in
+
+# in integer :
+
+lst = [1,2,3,4,5]
+
+print(5 in lst)
+print(6 in lst)
+
+
+# in string:
+ 
+name = "vikas"
+print("v" in name )
+print("h" in name)
+
+#>>>>>>>>>>>> not in
+ 
+# in integer:
+
+lst = [1,2,3,4,5]
+
+print(6 not in lst)
+print(5 not in lst)
+ 
+# in string :
+
+name = "vikas"
+
+print("V" not in name)
+print("v" not in name)
+
+#>>>>>> QUIZZZ
+
+numbers = [1,4,'2',8,5]
+
+print(2 in numbers)
+print(10 not in numbers)
