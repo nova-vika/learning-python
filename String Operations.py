9cb159042aaccf3437b4965c9c4cd0328f177183
Vikashini <ivikashinisuresh@gmail.com>
2025-07-28 14:23:40 +0530

    String Operations

---
+++ String Operations.py
@@ -0,0 +1,50 @@
+# STRING OPERATIONS :
+
+# CONCATENATION :
+       #symbol: +  
+  
+a="Hello"
+b="vikas"        # or  print("hello + vikas")
+print(a+b)    
+
+# REPETITION :
+    #  symbol: *
+
+print("hii!" * 5 )   # if we want space b\w each hii then code this: print("hii! "*5) .
+
+
+# LENGTH:
+    # len()
+
+"vikas"
+print(len("vikas"))
+
+# INDEXING:
+   # symbol: []
+
+"vikas"
+print("vikas"[0])  
+
+
+# SLICING:
+  # [start:end]
+
+"vikas"
+print("vikas"[0:2])  
+
+# MEMBERSHIP CHECK:
+    
+"vikas"
+print("a" in "vikas")   
+print("h" in "vikas")
+
+# UPPER CASE:
+
+"vikas"
+print("vikas".upper())
+
+# LOWER CASE:
+
+"VIKAS"
+print("VIKAS".lower())
+
