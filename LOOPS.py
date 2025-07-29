daf995ec69850748fbb17da259d5fa5588d1d23e
Vikashini <ivikashinisuresh@gmail.com>
2025-07-29 13:25:59 +0530

    LOOPS

--- LOOPS.py
+++ LOOPS.py
@@ -65,7 +65,56 @@ for fruits in fruits:
            # it keeps checking and running again and again until the condition become false.
 
                           # EXAMPLE:
+
+
 count = 1                                     # IMPORTANT : 
 while count <= 5:                                  # ALWAYS UPDATING THE VARIABLE (LIKE COUNT+=1)
     print(count)                                   # OR IT BECOMES AN INFINITE LOOP.
     count += 1 
+#___________________________________________________________________________________________________-
+
+                       # IF-ELSE IN FOR AND WHILE LOOP 
+                          # it is very important when u want to perform diff actions 
+                          # depending on conditions while looping .
+
+   # FOR LOOP:
+
+for i in range(5):
+ if i%2==0:
+    print(i,"is even")
+ else:
+    print(i,"is odd")   
+
+   # WHILE LOOP :
+
+count = 1 
+while count<=5:
+  if count==3:   
+    print("found 3!")
+  else:
+    print("not 3:",count)
+  count += 1
+
+#________________________________________________________________________________________________
+
+                            # NESTED IF :
+
+# it is mostly use to form patterns
+
+# EXAMPLE : 
+
+n = int(input("Enter n:"))
+for i in range(1 , n+1):
+  for j in range(1 , i+1):
+    print(j, end=" ")
+  print()  # Move to the next line after each row
+
+  
+
+
+   
+
+      
+         
+
+                           
