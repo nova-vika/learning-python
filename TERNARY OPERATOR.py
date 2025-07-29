4f9ccea7cad3e3cd73df4c0e887969dce00d5ea6
Vikashini <ivikashinisuresh@gmail.com>
2025-07-29 08:16:32 +0530

    TERNARY OPERATORS

---
+++ TERNARY OPERATOR.py
@@ -0,0 +1,21 @@
+#TERNARY OPERATOR :
+
+     # it is a on line shortened for an if-else statement . 
+
+     # SYNTAX :
+        # value_if_true if condition else value_if_false
+
+    # it is best when you need simple decision one line. 
+
+    # avoid if the logic is too compelex . keep your code readable. 
+
+# EXAMPLE :
+        
+age = 18
+print("adult" if age>=18 else"minor")        
+
+
+# finding odd or even OR EVEN :
+
+num = int(input("Enter a number:"))
+print('even'if num%2==0 else"odd")
